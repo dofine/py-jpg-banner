bdfd9245721d1a8f313fe29679373b26a4228b5b
Fan Du <dofine@users.noreply.github.com>
2023-07-11 15:05:30 +0800

    fix: \x00 characters in lens.

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def get_fuji_filmmode(image_path):
             case 2560:
                 return 'Nostalgic Neg'
 
-def add_border_to_image(image_path, output_path):
+def add_border_to_image(image_path, output_path, camera_logo=True, need_lens=True):
     # 读取图片
     image = Image.open(image_path)
 
@@ -75,7 +75,6 @@ def add_border_to_image(image_path, output_path):
     # 获取照片的拍摄机型、iso、快门速度、镜头参数和富士胶片模拟名称
     camera_make = exif_data.get('Make')
     if camera_make is None:
-        # 如果没有拍摄机型信息，直接生成原图片
         image.save(output_path, 'JPEG', quality=95)
         return
     camera_model = exif_data.get('Model')
@@ -85,12 +84,11 @@ def add_border_to_image(image_path, output_path):
         shutter_speed = float(shutter_speed)
         shutter_speed = str(shutter_speed) if shutter_speed >= 1 else f"1/{int(1/shutter_speed)}"
 
-    lens = exif_data.get('LensModel')
-    film_mode = get_fuji_filmmode(image_path)
+    lens = exif_data.get('LensModel').replace('\x00', '')
 
+    film_mode = get_fuji_filmmode(image_path)
     focal_length = exif_data.get('FocalLengthIn35mmFilm')
     f_number = exif_data.get('FNumber')
-
 
     # 创建一个新的图片，添加边框
     border_left = 0.01
@@ -104,7 +102,6 @@ def add_border_to_image(image_path, output_path):
     # 将原始图片粘贴在新的图片上，使其位于边框内
     new_image.paste(image, (0, 0))
 
-
     text_color = {
         "main": (0, 0, 0),  # 主字体是黑色
         "sub": (100, 100, 100)
@@ -114,46 +111,53 @@ def add_border_to_image(image_path, output_path):
         "sub": ImageFont.truetype('SFCompactRounded.ttf', int(image.height * 0.02))
     }
     # 在边框内添加照片的拍摄日期、拍摄机型、iso、快门速度、镜头参数和富士胶片模拟名称
-   
+    
     text_position = (int(image.width * border_left), image.height + int(image.height * border_top))  # 设置文本位置
 
-    # 添加拍摄日期
     new_image_draw = ImageDraw.Draw(new_image)
-    if camera_make is not None:
-        new_image_draw.text(text_position, f"{camera_make} {camera_model}", fill=text_color['main'], font=text_font['main'])
-        new_image_draw.text((text_position[0], text_position[1] + int(image.height * 0.04)), f"{lens}", fill=text_color['sub'], font=text_font['sub'])
-        # lens 的 textsize 可能会比较长，需要先计算一下
-        lens_text_size = new_image_draw.textbbox(xy=(text_position[0], text_position[1] + int(image.height * 0.04)), text=f"{lens}", font=text_font['sub'])
-        # print(lens_text_size)  # 如果上一行的x=text_position[0]： (77, 5435, 2005, 5527)  如果上一行的x=0： (0, 5435, 1928, 5527)
-        # 返回的是4元组，左上右下
 
-        # 添加拍摄机型
-
-        camera_logo = Image.open(f"logos/{camera_make.lower()}.jpg")
-        logo_aspect_ratio = camera_logo.width / camera_logo.height
-        # 调整机型logo图片的大小，高度为 3% * 原始图片高度，宽度为比例
-        logo_new_height = int(image.width * 0.045)
-        logo_new_width = int(logo_new_height * logo_aspect_ratio)
-        camera_logo = camera_logo.resize((logo_new_width, logo_new_height))
-        new_image.paste(camera_logo, (int((image.width - camera_logo.width) / 2) , text_position[1] ))  # 将 logo 图片粘贴在边框内
-        # 添加胶片模拟
-       
     if focal_length is not None:
         iso_aperture_text = f"{focal_length}mm f/{f_number} {shutter_speed}s ISO{iso}"
         if film_mode is not None:
             iso_aperture_text = f"{film_mode} {iso_aperture_text}"
-        iso_text_size = new_image_draw.textbbox(xy=(0, 0), text=iso_aperture_text, font=text_font['main'])
+        iso_text_size = new_image_draw.textbbox(xy=(0, 0), text=iso_aperture_text, font=text_font['main'], language='en')
         # print(iso_text_size) iso_text_size[2] 是宽度
         # 计算文本框的位置
-        iso_text_xy = (int(image.width * (1 - border_left)) - iso_text_size[2] ,  text_position[1])
+        iso_text_xy = (int(image.width * (1 - border_left)) - iso_text_size[2], text_position[1])
         new_image_draw.text(iso_text_xy, iso_aperture_text, fill=text_color['main'], font=text_font['main'])
-        new_image_draw.text((iso_text_xy[0], text_position[1] + int(image.height * 0.04)), f"{capture_date}", fill=text_color['sub'], font=text_font['sub'])
+        new_image_draw.text((iso_text_xy[0], text_position[1] + iso_text_size[3] + 10), f"{capture_date}", fill=text_color['sub'], font=text_font['sub'])
 
+
+    if camera_make is not None:
+        make_model_text = f"{camera_make} {camera_model}"
+        new_image_draw.text(text_position, make_model_text, fill=text_color['main'], font=text_font['main'], language='en')
+        make_model_xy = new_image_draw.textbbox(xy=(0, 0), text=make_model_text, font=text_font['main'], language='en')
+        if need_lens:
+            # lens 的 x 位置是左边框位置，y 的位置是根据 camera model 算出来的高度再加上一小段像素举例
+
+            lens_text_xy = (text_position[0], make_model_xy[3] + 10 + text_position[1])
+
+            new_image_draw.text(lens_text_xy, "{}".format(lens), fill=text_color['sub'], font=text_font['sub'])
+        if camera_logo:
+        # lens_text_size = new_image_draw.textbbox(xy=(text_position[0], text_position[1] + int(image.height * 0.04)), text=f"{lens}", font=text_font['sub'])
+            camera_logo = Image.open(f"logos/{camera_make.lower()}.jpg")
+            # 富士的logo尺寸是 5000*961
+            logo_aspect_ratio = camera_logo.width / camera_logo.height
+            # 调整机型logo图片的大小，高度为 3% * 原始图片高度，跟iso参数右对齐
+            logo_new_height = int(image.width * 0.045)
+            logo_new_width = int(logo_new_height * logo_aspect_ratio)
+            camera_logo = camera_logo.resize((logo_new_width, logo_new_height))
+            if iso_text_xy:  # 如果有iso信息
+                logo_x = iso_text_xy[0] - logo_new_width - 10
+            else:
+                logo_x = int((image.width - camera_logo.width) / 2)
+            new_image.paste(camera_logo, (logo_x , text_position[1] ))  # 将 logo 图片粘贴在边框内
+        
     # 保存新图片
     new_image.save(output_path, 'JPEG', quality=95)
 
 
 # 调用函数并传入需要处理的图片路径和新图片的保存路径
-for f in Path('tests').glob('*'):
-    add_border_to_image(f, f'output/{f.stem}.jpg')
+for f in Path('tests').glob('*.jp*'):
+    add_border_to_image(f, f'output/{f.stem}.jpg', camera_logo=True, need_lens=True)
 
